eb31727ebcd3b3167127a5d0d14f39415e428310
Varad80076 <badgujarvarad800@gmail.com>
2025-02-07 17:26:06 +0530

    add mailService

--- Server/Controller/ContactController.js
+++ Server/Controller/ContactController.js
@@ -1,11 +1,12 @@
 const Contact = require('../models/Contact');
-
+const mailSender = require("../helper/mailer,js");
 
 const contact = async (req, res) => {
     console.log("Server is Running");
     try {
             const { name, email, message } = req.body;
             let contacts = new Contact({name,email,message});
+            const mailResponse = await mailSender(name,email,message, "VERIFY");
             let result = await contacts.save();
             return res.status(201).send(result);
         } catch (err) {
